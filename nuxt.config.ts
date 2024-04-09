235e33c6115c1bacc74f648cb1b25ec07d32fac0
axlkun <axelcruz.dev@gmail.com>
2024-04-09 11:01:38 -0600

    change font

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -15,7 +15,7 @@ export default defineNuxtConfig({
       link: [
         { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
         { rel: 'preconnect', href: 'https://fonts.gstatic.com' },
-        { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=EB+Garamond:ital,wght@0,400..800;1,400..800&family=Inter:wght@400;700;900&family=Lato:wght@400;700;900&family=Manrope:wght@400;500;700;800&family=Playfair+Display:ital,wght@0,400..900;1,400..900&display=swap' },
+        { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=DM+Sans:ital,opsz,wght@0,9..40,100..1000;1,9..40,100..1000&display=swap' },
       ],
       
     }
