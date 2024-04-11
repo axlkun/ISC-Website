8681ceed4885c879c79868122894cc58b5056458
axlkun <axelcruz.dev@gmail.com>
2024-04-10 20:12:54 -0600

    add height

--- components/Parallax.vue
+++ components/Parallax.vue
@@ -1,6 +1,6 @@
 <template>
 
-    <v-parallax src="/assets/parallax.webp" height="400">
+    <v-parallax src="/assets/parallax.webp" height="500">
         <div class="d-flex flex-column fill-height justify-center align-center text-white ma-3 text-center">
             <h2 class="text-h4 font-weight-thin mb-4">
                 ISC: Ingeniería Servicios & Construcción
