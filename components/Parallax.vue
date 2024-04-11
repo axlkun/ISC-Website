b10ebfa7708e91e908eb4e826ae5cd62908bcdd0
axlkun <axelcruz.dev@gmail.com>
2024-04-10 20:01:42 -0600

    new parallax image

--- components/Parallax.vue
+++ components/Parallax.vue
@@ -1,6 +1,6 @@
 <template>
 
-    <v-parallax src="/assets/edificio-4.webp" height="400">
+    <v-parallax src="/assets/parallax.webp" height="400">
         <div class="d-flex flex-column fill-height justify-center align-center text-white ma-3 text-center">
             <h2 class="text-h4 font-weight-thin mb-4">
                 ISC: Ingeniería Servicios & Construcción
