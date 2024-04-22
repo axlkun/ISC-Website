8fff1d78dc788ad5983d15db0f97111ce7a28987
axlkun <axelcruz.dev@gmail.com>
2024-04-22 11:31:30 -0600

    add vue router

--- app.vue
+++ app.vue
@@ -1,29 +1,16 @@
 <template>
   <v-app id="app">
 
-    <myHeader/>
-    <myHero/>
-    <mySobreNosotros/>
-    
-    <myServicios/>
-    <myNuestraFormula/>
-    <myProyectos/>
-    <myParallax/>
-    <myContact/>
-    <myFooter/>
-    <!-- .... -->
+    <myHeader></myHeader>
+    <main>
+      <router-view></router-view>
+    </main>
+    <myFooter></myFooter>
   </v-app>
 </template>
 
 <script setup>
 import myHeader from './components/Header.vue';
-import myHero from './components/Hero.vue';
-import mySobreNosotros from './components/SobreNosotros.vue';
-import myNuestraFormula from './components/NuestraFormula.vue';
-import myServicios from './components/Servicios.vue';
-import myProyectos from './components/Proyectos.vue';
-import myParallax from './components/Parallax.vue';
-import myContact from './components/Contact.vue';
 import myFooter from './components/Footer.vue';
 
 
