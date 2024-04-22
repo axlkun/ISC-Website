5b8ed95e5de4335daf5e43652a8a0216646e1782
axlkun <axelcruz.dev@gmail.com>
2024-04-22 11:31:23 -0600

    create index page

---
+++ pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+    <v-sheet>
+        <myHero/>
+        <mySobreNosotros/>
+        <myServicios/>
+        <myNuestraFormula/>
+        <myProyectos/>
+        <myParallax/>
+        <myContact/>
+    </v-sheet>
+  </template>
+  
+  <script setup>
+  import myHero from '../components/Hero.vue';
+  import mySobreNosotros from '../components/SobreNosotros.vue';
+  import myNuestraFormula from '../components/NuestraFormula.vue';
+  import myServicios from '../components/Servicios.vue';
+  import myProyectos from '../components/Proyectos.vue';
+  import myParallax from '../components/Parallax.vue';
+  import myContact from '../components/Contact.vue';
+  
+  </script>
