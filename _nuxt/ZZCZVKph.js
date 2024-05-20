0c9db10e84d07ac7c2f808feff270e8fadd63b59
axlkun <93900267+axlkun@users.noreply.github.com>
2024-05-20 16:25:08 +0000

    Build: (13bbeb2) remove baseUrl

---
+++ _nuxt/ZZCZVKph.js
@@ -0,0 +1,45 @@
+import{i as l}from"./BglBUANq.js";import{_ as d,L as p,A as a,t as r,z as e,v as u,N as m,M as g,x as o,ag as y,y as f,C as b,D as v}from"./tyERN7c-.js";import{m as _,a as h}from"./DT24bDAD.js";import{V as i}from"./B9oFmR4t.js";import"./M6R3e8Nf.js";const q=""+new URL("supervision-obra.uaCFMfn1.webp",import.meta.url).href,z=""+new URL("obra-civil.BgvTbDeS.webp",import.meta.url).href,I=""+new URL("diseño-arquitectonico.65QdiIqA.webp",import.meta.url).href,N=""+new URL("ingenierias.BGOgp_Ku.webp",import.meta.url).href,w=""+new URL("calculos-estructurales.B8D-SVYk.webp",import.meta.url).href,x=""+new URL("metodologia-pmi.GQlUyXWc.webp",import.meta.url).href,c=s=>(b("data-v-af0d5b93"),s=s(),v(),s),C=c(()=>o("p",null,"Servicios",-1)),S=c(()=>o("h1",null,"Ingeniería, Construcción y Diseño.",-1)),D=c(()=>o("p",null,"En ISC ofrecemos un servicio integral para todos los requerimientos de cualquier tipo de desarrollo, desde residenciales hasta urbanos. Estamos comprometidos con ofrecer la mayor calidad en cada uno de ellos",-1)),M=["src"],E=["innerHTML"],L={__name:"servicios",setup(s){const t=[{title:"Gerencia y Administración de proyectos",icon:"mdi-bag-checked",image:l,description:`<p>Nos especializamos en ofrecer soluciones integrales para la gestión eficiente de proyectos de
+                        construcción. Desde la etapa inicial de pre-construcción hasta la conclusión del proyecto,
+                        nuestro equipo de expertos se encarga de coordinar cada fase con meticulosidad y
+                        profesionalismo.</p>
+
+                    <p>Nuestros servicio abarca desde la planificación estratégica y la gestión de recursos hasta el
+                        control de calidad y el cumplimiento de plazos. Nos comprometemos a mantener una comunicación
+                        transparente y constante con nuestros clientes, proporcionando informes detallados y
+                        actualizaciones regulares sobre el progreso del proyecto.</p>`},{title:"Ejecución y Supervisión de obra",icon:"mdi-office-building-cog",image:q,description:`<p>
+    Nuestro equipo altamente capacitado coordina de manera efectiva todos los aspectos operativos para garantizar que la visión del proyecto se materialice con excelencia y puntualidad.
+</p>
+<p>
+    Desde el inicio de la construcción, nuestra labor va más allá de la simple ejecución; nos esforzamos por fusionar la experiencia técnica con una visión estratégica. Coordinamos minuciosamente a los equipos de trabajo, supervisando cada detalle con atención meticulosa para asegurar la coherencia con los planos y especificaciones.
+</p>`},{title:"Obra civil",icon:"mdi-bridge",image:z,description:`<p>
+    Enfocados en el desarrollo de proyectos urbanos y de infraestructura a gran escala, nuestro equipo se compromete a priorizar los más altos estándares de seguridad y durabilidad en cada etapa del proceso.
+</p>
+<p>
+    Nos dedicamos a concebir y ejecutar obras civiles que no solo transforman entornos, sino que también enriquecen comunidades. Desde la planificación inicial hasta la implementación final, nuestra atención se centra en la creación de estructuras sólidas y funcionales que perduren en el tiempo.
+</p>`},{title:"Diseño arquitectónico",image:I,icon:"mdi-home-city",description:`<p>
+    Enfocados en la creación de espacios innovadores y funcionales, nuestro equipo especializado se dedica a materializar la visión y las necesidades de nuestros clientes a través de diseños arquitectónicos inspiradores.
+</p>
+<p>
+    Cada proyecto es abordado con creatividad y atención al detalle, buscando no solo cumplir con los requisitos funcionales, sino también superar las expectativas estéticas y emocionales de nuestros clientes y usuarios finales.
+</p>`},{title:"Desarrollo de Ingenierías",icon:"mdi-cog",image:N,description:`
+    Nos especializamos en el diseño y desarrollo de ingenierías en una amplia gama de disciplinas, incluyendo sistemas de bajo voltaje, eléctricos, hidrosanitarios, mecánicos, gases medicinales y sistemas HVAC.
+</p>
+<p>
+    Nuestro equipo de ingenieros altamente calificados trabaja en estrecha colaboración para crear soluciones técnicas innovadoras y eficientes que cumplan con los más altos estándares de calidad y seguridad.
+</p>`},{title:"Cálculos Estructurales",icon:"mdi-calculator",image:w,description:`<p>
+    Nuestro equipo de ingenieros especializados se dedica a realizar análisis detallados para garantizar que cada estructura diseñada cumpla con los más altos estándares de seguridad y resistencia requeridos.
+</p>
+<p>
+    Nuestro enfoque se basa en la combinación de experiencia técnica y conocimientos especializados. Consideramos factores como la carga, la resistencia del material, la estabilidad y la seguridad sísmica para garantizar que cada diseño estructural sea sólido, seguro y funcional.
+</p>
+<p>
+    Utilizando herramientas avanzadas y técnicas de modelado estructural, llevamos a cabo una evaluación exhaustiva de cada componente y sistema estructural. Esto nos permite asegurar la integridad estructural del proyecto.
+</p>`},{title:"Gestion de proyectos con metodologia PMI",icon:"mdi-calendar-clock",image:x,description:`<p>
+    Adoptamos un enfoque de gestión de proyectos basado en la metodología PMI (Project Management Institute), reconocida internacionalmente por su eficacia en garantizar una gestión eficiente del tiempo, los recursos y el alcance del proyecto.
+</p>
+<p>
+    Nuestro equipo de gestión de proyectos está altamente capacitado y certificado en la metodología PMI, lo que nos permite aplicar las mejores prácticas y estándares de la industria en cada proyecto que emprendemos.
+</p>
+<p>
+    Nos comprometemos a seguir los principios fundamentales de la metodología PMI, incluida la planificación detallada, la gestión de riesgos, el control del cambio y la comunicación efectiva. Estos principios nos ayudan a mantenernos en el camino correcto, minimizar los riesgos y asegurar la entrega exitosa del proyecto dentro del tiempo y el presupuesto establecidos.
+</p>`}];return(j,U)=>(r(),p(i,{class:"servicios"},{default:a(()=>[e(i,{class:"heading"},{default:a(()=>[C,S,D]),_:1}),e(i,{class:"list-services"},{default:a(()=>[(r(),u(g,null,m(t,n=>e(i,{class:"service mb-16"},{default:a(()=>[e(i,{class:"service-image"},{default:a(()=>[o("img",{src:n.image,alt:"Imagen Sobre Nosotros",loading:"lazy"},null,8,M)]),_:2},1024),e(i,{class:"service-text"},{default:a(()=>[e(y,{color:"#bc8c34",icon:n.icon,size:"large"},null,8,["icon"]),o("h2",null,f(n.title),1),o("div",{class:"service-description",innerHTML:n.description},null,8,E)]),_:2},1024)]),_:2},1024)),64))]),_:1}),e(_),e(h)]),_:1}))}},R=d(L,[["__scopeId","data-v-af0d5b93"]]);export{R as default};
