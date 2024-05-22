02b17ed5837033a1425b1579c0b85059c4e45efb
axlkun <93900267+axlkun@users.noreply.github.com>
2024-05-22 05:37:36 +0000

    Build: (5401785) set api url

--- _nuxt/BglBUANq.js
+++
@@ -1,1 +0,0 @@
-import"./tyERN7c-.js";const r=""+new URL("sobre-nosotros1.BWFrlHom.webp",import.meta.url).href;export{r as i};
---
+++ _nuxt/ChJH3uKZ.js
@@ -0,0 +1,1 @@
+import"./BrtOjDhE.js";const r=""+new URL("sobre-nosotros1.BWFrlHom.webp",import.meta.url).href;export{r as i};
